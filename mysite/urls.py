1542852f1f602489a0c0b5b1d5aa95bf0274102a
Sasi Mandava <sasimandava6@gmail.com>
2017-03-07 19:09:29 -0800

    Second Commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -29,7 +29,17 @@ urlpatterns = [
 ]
 
 
+# Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
